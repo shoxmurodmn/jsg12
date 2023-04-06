a6230e9be5e629cb33758168b01370cf169450c0
Islombek-FD <islombekqurbonovfd@gmail.com>
2023-04-06 15:28:53 +0500

    test

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log(111);
+// console.log(111);
+// console.log(111);
+// console.log("Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe distinctio repellat reprehenderit dolorem accusamus commodi unde assumenda pariatur quod a?nod");
+// console.log("Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe distinctio repellat reprehenderit dolorem accusamus commodi unde assumenda pariatur quod a?nod");
+// console.log('Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe distinctio repellat reprehenderit dolorem accusamus commodi unde assumenda pariatur quod a?')
+// let it = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe distinctio repellat reprehenderit dolorem accusamus commodi unde assumenda pariatur quod a?'
+// console.log(it)
+
+// let son = 22
+// console.log(son);
+// // console.log(2**53 + 1)
+// let bi = 9007199254740992n
+
+// console.log(bi + 1n)
+
+// let str = " salom dunyo"
+
+// console.log(str)
+// console.log(true);
+// console.log(false);
+
+// let top = null
+// let un ;
+
+// console.log(top);
+// console.log(typeof top);.
+// let un;
+
+// console.log(typeof un);
+// symbols
+
